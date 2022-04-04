f03bedca1b0ed101342cd8188df052e358ee2fca
kafkaesc <jared.a.hettinger@gmail.com>
2022-04-03 21:35:53 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Birdblock
+
+This project is a basic demonstration of a blockchain implementation in JavaScript. SHA256 is used for the block hashes, and the blockchain is designed to track blocks containing details about various birds.
+
+Built by Jared Hettinger.
---
+++ main.js
@@ -0,0 +1,100 @@
+const SHA256 = require('crypto-js/sha256');
+
+class BirdBlock {
+    constructor(species, index, timestamp, previousHash = '') {
+        let bird = null;
+        if (species === 'bluebird')
+            bird = { call: 'Tweet!', species: 'Bluebird' };
+        else if (species === 'crow')
+            bird = { call: 'Caw caw', species: 'Crow' };
+        else if (species === 'duck')
+            bird = { call: 'Quack quack', species: ' Duck' };
+        else if (species === 'goose')
+            bird = { call: 'Honk honk', species: 'Goose' };
+        else if (species === 'impostor')
+            bird = { call: 'Moo', species: 'Impostor' };
+        else if (species === 'mockingbird')
+            bird = { call: 'Ya call this a block?', species: 'Mockingbird' };
+        else if (species === 'owl')
+            bird = { call: 'Hoot hoot', species: 'Owl' };
+        else if (species === 'raven')
+            bird = { call: 'Nevermore', species: 'Raven' };
+        else if (species === 'roadrunner')
+            bird = { call: 'Beep beep', species: 'Roadrunner' };
+        else if (species === 'turkey')
+            bird = { call: 'Gobble gobble', species: 'Turkey'};
+        else
+            bird = { call: '...', species: '...' };
+
+        this.data = bird;
+        this.index = index;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return SHA256(
+            this.data.call +
+            this.data.species + 
+            this.index + 
+            this.previousHash + 
+            this.timestamp
+        ).toString();
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.generateGenesisBlock()];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    checkChainValidity() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash())
+                return false;
+            if (previousBlock?.hash !== previousBlock.calculateHash())
+                return false;
+            if (currentBlock.previousHash !== previousBlock.hash)
+                return false;
+        }
+
+        return true;
+    }
+
+    generateGenesisBlock() {
+        return !this.chain
+            ? new BirdBlock(
+                null,
+                0,
+                new Date().toISOString(),
+                '0')
+            : null;
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+}
+
+let birdblock = new Blockchain();
+birdblock.addBlock(new BirdBlock('raven', 1, new Date().toISOString()));
+birdblock.addBlock(new BirdBlock('crow', 2, new Date().toISOString()));
+
+console.log('Original blockchain with a raven and crow added.')
+console.log('Is the blockchain valid?: ', birdblock.checkChainValidity());
+console.log('');
+
+birdblock.chain[1].data.species = 'goose';
+console.log('Going goose mode on a previous block.');
+console.log('Is the blockchain valid?: ', birdblock.checkChainValidity());
+console.log();
